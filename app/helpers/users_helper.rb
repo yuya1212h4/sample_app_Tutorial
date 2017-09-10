6ccf90bd744f7c283c41b3460ebd9090e0922c80
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-11 07:01:46 +0900

    def grabatar_for on users_helpler.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  # 引数で与えられたユーザーのGravatar画像を返す
+  # def gravatar_for(user, options = { size: 80 })# オプション引数
+  def gravatar_for(user, size: 80)# キーワード引数
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # size = options[:size] # オプション引数を設定したときに必要
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
