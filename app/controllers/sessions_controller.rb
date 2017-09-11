2452d6a596700cc8a34d6f9779071ef2ca6acdbb
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-11 23:08:07 +0900

    update flash.now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
 
     else
-      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
       render 'new'
     end
   end
