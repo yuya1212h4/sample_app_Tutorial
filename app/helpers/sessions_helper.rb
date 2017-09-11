282cca4a2e64844affb7844eb6cb7c324f5e3b7c
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-11 23:32:06 +0900

    current_user and logged_in? method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,14 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザーがログインしていれば、ture、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
