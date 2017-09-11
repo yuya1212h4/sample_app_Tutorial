6ad99fd198d5d51414ab0b3763c05bfdbe29ce8c
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-11 14:39:56 +0900

    add create method on sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
