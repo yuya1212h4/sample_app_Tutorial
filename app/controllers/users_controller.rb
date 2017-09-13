cbe4cb8594cfcfb4c5cfbd15e457866da682d71c
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-14 00:16:15 +0900

    add index action on users_contoller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+  end
 
   def show
     @user = User.find(params[:id])
@@ -43,6 +46,7 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
