69618c2a09a3d8ee0873be60752f401558e82a40
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-21 18:19:10 +0900

    create update action on password_resets_contoller.rb

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -22,7 +22,24 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 
   def get_user
     @user = User.find_by(email: params[:email])
