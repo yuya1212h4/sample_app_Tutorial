44ead62ac807acc29693d6dd05410ac6bb49faea
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-21 17:21:09 +0900

    fix password_resets_controller.rb

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,7 +10,7 @@ class PasswordResetsController < ApplicationController
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
     else
-      flash.new[:danger] = "Email address not found"
+      flash.now[:danger] = "Email address not found"
       render 'new'
     end
   end
