45370cbcfed7fe6b70a10ec3bb5fa26b392abdde
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-21 17:42:53 +0900

    add get_user and valid_user and check_expiration method on passwordresetscontroller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -16,5 +20,26 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # 有効なユーザーかどうか確認する
+  def valid_user
+    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
+
+  # トークンが期限切れがどうか確認する
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
   end
 end
