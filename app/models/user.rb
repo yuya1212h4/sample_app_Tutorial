e6599ef872cee13916c11776d577b9da5d0778ad
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-09-24 18:33:51 +0900

    update app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,9 +76,9 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-  # 試作feedの定義
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
   end
 
   # ユーザーをフォローする
